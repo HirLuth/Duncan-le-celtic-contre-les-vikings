65aa87f3e97fa17a2338149bdd13b7273a216324
Ewenn <ewenn-brieuc@orange.fr>
2022-09-27 11:45:42 +0200

    UI

--- Le projet Unity/Assets/Scripts/UIManager.cs
+++ Le projet Unity/Assets/Scripts/UIManager.cs
@@ -8,16 +8,13 @@ public class UIManager : MonoBehaviour
     public List<GameObject> listOfPositions;
     public List<GameObject> listOfWeapons;
     public List<GameObject> possessedWeapons;
-
-    private void Start()
-    {
-        AddWeapon(ItemManager.WeaponsType.Javelot);
-    }
+    
 
     public void AddWeapon(ItemManager.WeaponsType weaponsType)
     {
-        int placeInThelist = possessedWeapons.Count + 1;
+        int placeInThelist = possessedWeapons.Count;
         GameObject newWeapon = Instantiate(listOfWeapons[(int)weaponsType]);
+        newWeapon.transform.parent = gameObject.transform;
         possessedWeapons.Add(newWeapon);
         newWeapon.transform.position = listOfPositions[placeInThelist].transform.position;
     }
