b29e8b8bb14c01054683be10065710b937e6800a
Ewenn <ewenn-brieuc@orange.fr>
2022-09-27 11:34:50 +0200

    Bim

--- Le projet Unity/Assets/Scripts/UIManager.cs
+++ Le projet Unity/Assets/Scripts/UIManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,11 @@ public class UIManager : MonoBehaviour
     public List<GameObject> listOfPositions;
     public List<GameObject> listOfWeapons;
     public List<GameObject> possessedWeapons;
+
+    private void Start()
+    {
+        AddWeapon(ItemManager.WeaponsType.Javelot);
+    }
 
     public void AddWeapon(ItemManager.WeaponsType weaponsType)
     {
