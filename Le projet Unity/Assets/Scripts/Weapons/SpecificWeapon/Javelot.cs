e6564967c4c5b12abcce7a133ba7b7445a16e8be
PHILIPPON <m.philippon@rubika-edu.com>
2022-09-27 12:13:30 +0200

    Début Code Javelot

---
+++ Le projet Unity/Assets/Scripts/Weapons/SpecificWeapon/Javelot.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace Weapons.SpecificWeapon
+{
+    public class Javelot : MonoBehaviour
+    {
+        [SerializeField]
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
