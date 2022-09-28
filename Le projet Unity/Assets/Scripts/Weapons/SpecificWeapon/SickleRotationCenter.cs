1304eeed900385a013bf4e79c9782d68f8a1d5d9
PHILIPPON <m.philippon@rubika-edu.com>
2022-09-28 12:22:51 +0200

    Serpe + workspace dans le GitIgnore

--- Le projet Unity/Assets/Scripts/Weapons/SpecificWeapon/SickleRotationCenter.cs
+++ Le projet Unity/Assets/Scripts/Weapons/SpecificWeapon/SickleRotationCenter.cs
@@ -12,6 +12,5 @@ public class SickleRotationCenter : MonoBehaviour
     {
         _currentPointForAngle+=sickleStat.projectileSpeed+10;
         transform.localRotation = Quaternion.Euler(0,0,_currentPointForAngle);
-        //transform.localScale = new Vector3(CharacterController.instance.transform.localScale.x, 1, 1);
     }
 }
