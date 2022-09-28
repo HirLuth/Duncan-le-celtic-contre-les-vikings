6d2db0d9044a45c94983ae419e282567ab20a5e0
Ewenn <ewenn-brieuc@orange.fr>
2022-09-28 12:05:06 +0200

    Spear fonctionne

--- Le projet Unity/Assets/Scripts/Weapons/SpecificWeapon/Spear.cs
+++ Le projet Unity/Assets/Scripts/Weapons/SpecificWeapon/Spear.cs
@@ -22,10 +22,14 @@ public class Spear : MonoBehaviour
 
     private void LaunchSpearProjectiles()
     {
-        GameObject javelot = Instantiate(projectile,CharacterController.instance.transform.position, Quaternion.Euler(0, 0,  90 - CharacterController.instance.listPositionDegree[CharacterController.instance.lookingAt]));
-        SpearProjectile projectileScriptReference = javelot.GetComponent<SpearProjectile>();
-        projectileScriptReference.speed = armes.projectileSpeed;
-        projectileScriptReference.damage = armes.damage;
-        projectileScriptReference.direction = Quaternion.AngleAxis(- CharacterController.instance.listPositionDegree[CharacterController.instance.lookingAt], Vector3.forward ) * Vector3.up;
+        for (int i = 0; i < armes.numberOfProjectile; i++)
+        {
+            GameObject javelot = Instantiate(projectile,CharacterController.instance.transform.position + new Vector3(Random.Range(-1f,1f),Random.Range(-1f,1f),0), Quaternion.Euler(0, 0,  90 - CharacterController.instance.listPositionDegree[CharacterController.instance.lookingAt]));
+            SpearProjectile projectileScriptReference = javelot.GetComponent<SpearProjectile>();
+            projectileScriptReference.speed = armes.projectileSpeed;
+            projectileScriptReference.damage = armes.damage;
+            projectileScriptReference.direction = Quaternion.AngleAxis(- CharacterController.instance.listPositionDegree[CharacterController.instance.lookingAt], Vector3.forward ) * Vector3.up;
+        }
+        
     }
 }
